4e9a95ef97055988182defd239762321c304bbec
laox <xie5997231@gmail.com>
2020-01-14 21:16:10 +0800

    demo 尝试了worker

---
+++ canvas/offsetCanvas/index.js
@@ -0,0 +1,9 @@
+/**
+
+ * @author xieyanghao
+
+ * @date 2020-2020/1/14-4:02 下午
+
+ */
+“use
+strict”
---
+++ canvas/offsetCanvas/index2.js
@@ -0,0 +1,5 @@
+/**
+ * @author xieyanghao
+ * @date 2020-2020/1/14-5:46 下午
+ */
+"use strict";
