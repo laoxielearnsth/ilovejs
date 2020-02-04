16243c43cfd597da2dea9e6ddbb77cd4b90807db
laox <xie5997231@gmail.com>
2020-02-04 22:12:40 +0800

    当传染率不为部位100%时的传染模拟

---
+++ canvas/transwithrate/index.js
@@ -0,0 +1,31 @@
+/**
+ * @author xieyanghao
+ * @date 2020-2020/2/4-2:38 下午
+ */
+"use strict";
+
+let canvas = document.getElementById('canvas');
+let offscreen = canvas.transferControlToOffscreen();
+
+let worker = new Worker("./worker.js");
+
+let rate = document.getElementById('rate');
+let box = document.getElementById('box');
+
+worker.postMessage({msg:'init', canvas: offscreen}, [offscreen]);
+
+function reset() {
+    worker.postMessage({msg: "reset"})
+}
+
+function step() {
+    worker.postMessage({msg: "step"})
+}
+
+function play() {
+    worker.postMessage({msg: "play"})
+}
+
+rate.addEventListener("change", (e)=>{
+    box.innerText = "rate:" + e.target.value;
+    worker.postMessage({msg: 'rate', rate: e.target.value});});
