a7028189f4f0596e7907730c024cd394cccd91c7
laoxielearnsth <xie5997231@gmail.com>
2020-08-06 23:35:22 +0800

    修改了一次通信的模型

---
+++ useful/example1.js
@@ -0,0 +1,47 @@
+/**
+ * @param e
+ * @returns {{status: boolean}|{result: *, status: boolean}}
+ */
+const handlerExample = (e) => {
+    const {cmd, data} = e.data;
+    if (cmd === 'condition') {
+        // handle data
+        return {
+            status: true,
+            result: data
+        }
+    } else {
+        return {
+            status: false,
+        };
+    }
+};
+
+/**
+ * @param targetWindow 目标窗口
+ * @param handler 处理器
+ * @param postMsg 传送的信息
+ * @returns {Promise<void>}
+ */
+const f = async (targetWindow, handler, postMsg) => {
+    const wrap = (fn, res, rej) => {
+        const wrapped = (e) => {
+            let r = fn(e);
+            if (r.status) {
+                window.removeEventListener('message', wrapped);
+                res(r.result)
+            }
+        };
+        return wrapped;
+    }
+
+    new Promise((resolve, reject) => {
+        const wrapped = wrap(handler, resolve, reject);
+
+        window.addEventListener('message', wrapped);
+    });
+
+    targetWindow.postMessage(postMsg, '*');
+}
+
+export default f;
